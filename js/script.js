932a083bd4256b766f8235781d9db9b30817622a
romanyears <47555868+romanyears@users.noreply.github.com>
2021-06-18 21:52:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,144 @@
+/* =========================================
+                Preloader
+============================================ */
+$(window).on('load', function () { // makes sure that whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+
+
+
+/* =========================================
+               Responsive Tabs
+============================================ */
+$(function () {
+
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+    });
+
+});
+
+
+/* =========================================
+               Portfolio
+============================================ */
+$(window).on('load', function () {
+
+    // Initialize Isotope
+    $("#isotope-container").isotope({});
+
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function () {
+
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+
+        // active button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+/* =========================================
+               Magnifier
+============================================ */
+$(function () {
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
+
+
+
+
+
+/* =========================================
+              Navigation
+============================================ */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+
+            // Show back to top button
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+
+            // Hide back to top button
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+// Smooth Scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get section id like #about, #servcies, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, "easeInOutExpo");
+
+    });
+
+});
+
+/* =========================================
+              Mobile Menu
+============================================ */
+$(function () {
+
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+
+});
+
